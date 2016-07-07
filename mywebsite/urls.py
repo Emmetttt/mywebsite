d74b4867bfa139883dac6d3d504bbf4199570482
emmetttt <emmettjd@hotmail.co.uk>
2016-07-07 17:39:34 +0100

    added basic blog url

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^home/', include('home.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    url(r'^blog/', include('blog.urls')),
 ]
