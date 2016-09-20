0e6a375fa6d41c3451426bd1d2f7051a2b6d2d3a
Emmett <emmettjd@hotmail.co.uk>
2016-09-20 13:02:55 +0000

    bugs

--- music_grapher/views.py
+++ music_grapher/views.py
@@ -15,76 +15,6 @@ import requests
 from .models import Band
 from .forms import BandForm
 
-# def RetrieveInfo(name):
-#     artistnamedash = name.replace(" ", "-")
-#     artistname = artistnamedash.lower()
-#     page = "http://www.metacritic.com/person/" + artistname
-
-#     headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5)'}
-#     urllibpage = requests.get(page,headers=headers)
-#     soup = BeautifulSoup(urllibpage.text, "lxml")
-#     mydivs = soup.findAll("table", { "class" : "credits person_credits" })
-#     mytrs = soup.findAll("tr")
-#     #print(mytrs)
-
-#     Months = {'Jan': '01',
-#           'Feb': '02',
-#           'Mar': '03',
-#           'Apr': '04',
-#           'May': '05',
-#           'Jun': '06',
-#           'Jul': '07',
-#           'Aug': '08',
-#           'Sep': '09',
-#           'Oct': '10',
-#           'Nov': '11',
-#           'Dec': '12'}
-
-
-#     albumNames = []
-#     albumScores = []
-#     albumYears = []
-
-#     for score in soup.findAll("span", { "class" : "metascore_w small release positive"}):
-#         albumScores.append(score.string)
-
-#     for year in soup.findAll("td", { "class" : "year" }):
-#         alyear = year.string
-#         albumYears.append(alyear.strip())
-
-#     i = 0
-
-#     for date in albumYears:
-#         for word, initial in Months.items():
-#             albumYears[i] = albumYears[i].replace(word, initial)
-#         if albumYears[i][4] == ',':
-#             albumYears[i] = albumYears[i][:3] + '0' + albumYears[i][3:]
-#         albumYears[i] = albumYears[i].replace(" ", "")
-#         albumYears[i] = albumYears[i].replace(",", "")
-#         albumYears[i] = albumYears[i][-4:] + '.' + albumYears[i][:4]
-#         albumYears[i] = float(albumYears[i])
-#         i = i + 1
-
-#     j = 0
-
-#     for score in albumScores:
-#         albumScores[j] = int(float(albumScores[j]))
-#         j = j + 1
-
-
-#     for name in soup.findAll("a", href=re.compile('^/music/')):
-#         albumNames.append(name.string)
-
-#     k = 0
-#     data = []
-#     for value in albumScores:
-#         #data.append([albumScores[k], albumYears[k]])
-#         data.append([albumYears[k], albumScores[k]])
-#         k = k + 1
-
-#     #all_together = list(zip(albumNames[2:], albumScores, albumYears))
-#     return albumNames, albumScores, albumYears, data
-
 def RetrieveInfo(name):
     name = name.lower() ##lowers all the capitals
     nameDashed = name.replace(' ', '-') ##replace spaces with -
@@ -135,7 +65,7 @@ def band_input(request):
         Bform = BandForm()
     return render(request, 'music_grapher/index.html', {'Bform': Bform})
 
- 
+
 
 
 def graph(request):
